e6896233c3ad1bc59e146baeebab4fe7eb823ec4
Eloy Coto <eloy.coto@acalustra.com>
2023-02-12 17:00:10 +0100

    new
    Signed-off-by: Eloy Coto <eloy.coto@acalustra.com>

--- workflow-service/src/test/java/com/redhat/parodos/config/SwaggerConfigTest.java
+++ workflow-service/src/test/java/com/redhat/parodos/config/SwaggerConfigTest.java
@@ -3,9 +3,6 @@ package com.redhat.parodos.config;
 import org.junit.Test;
 // import org.mockito.Mockito;
 
-// import com.redhat.parodos.workflows.work.Work;
-// import com.redhat.parodos.workflows.work.WorkContext;
-// import com.redhat.parodos.workflows.work.WorkReportPredicate;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class SwaggerConfigTest {
